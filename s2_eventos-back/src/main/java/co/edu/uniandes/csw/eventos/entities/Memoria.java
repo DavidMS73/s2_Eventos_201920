15ee3c28e5722f2588601590714aa379f722cf51
a.despres@uniandes.edu.co <>
2019-08-20 10:26:08 -0500

    Comienzo la clase Memoria

---
+++ s2_eventos-back/src/main/java/co/edu/uniandes/csw/eventos/entities/Memoria.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.eventos.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Memoria extends BaseEntity implements Serializable{
+    private string lugar;
+    
+    public String getLugar{
+        return lugar;
+    };
+    
+}
