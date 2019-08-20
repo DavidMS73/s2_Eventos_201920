61eb7b3b7f2ffc201138fc46db2a81948b8d62ef
Germán Martínez <gd.martinez@uniandes.edu.co>
2019-08-20 10:52:27 -0500

    Cree EventoEntity

---
+++ s2_eventos-back/src/main/java/co/edu/uniandes/csw/eventos/entities/EventoEntity.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.eventos.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Germán David Martínez Solano
+ */
+@Entity
+public class EventoEntity extends BaseEntity implements Serializable {
+       
+    private String nombre;
+    private String categoria;
+    private String descripcion;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private String detallesAdicionales;
+    private Integer entradasRestantes;
+    private String tipo;
+    private Boolean esPago;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the categoria
+     */
+    public String getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the fechaInicio
+     */
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    /**
+     * @param fechaInicio the fechaInicio to set
+     */
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    /**
+     * @return the fechaFin
+     */
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    /**
+     * @param fechaFin the fechaFin to set
+     */
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    /**
+     * @return the detallesAdicionales
+     */
+    public String getDetallesAdicionales() {
+        return detallesAdicionales;
+    }
+
+    /**
+     * @param detallesAdicionales the detallesAdicionales to set
+     */
+    public void setDetallesAdicionales(String detallesAdicionales) {
+        this.detallesAdicionales = detallesAdicionales;
+    }
+
+    /**
+     * @return the entradasRestantes
+     */
+    public Integer getEntradasRestantes() {
+        return entradasRestantes;
+    }
+
+    /**
+     * @param entradasRestantes the entradasRestantes to set
+     */
+    public void setEntradasRestantes(Integer entradasRestantes) {
+        this.entradasRestantes = entradasRestantes;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the esPago
+     */
+    public Boolean getEsPago() {
+        return esPago;
+    }
+
+    /**
+     * @param esPago the esPago to set
+     */
+    public void setEsPago(Boolean esPago) {
+        this.esPago = esPago;
+    }
+}
