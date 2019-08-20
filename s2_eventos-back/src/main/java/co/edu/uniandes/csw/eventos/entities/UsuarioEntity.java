fa12f4632bea1fa90f2d01b12d9d8fdee3eb9d05
d.betancurth@uniandes.edu.co <>
2019-08-20 15:15:53 -0500

    "Se creo la clase UsuarioEntity y su tabla"

---
+++ s2_eventos-back/src/main/java/co/edu/uniandes/csw/eventos/entities/UsuarioEntity.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.eventos.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+/**
+ *
+ * @author Daniel Beetancurth Dorado
+ */
+@Entity
+public class UsuarioEntity extends BaseEntity implements Serializable {
+    private Long identificador;
+    private String nombre;
+    private String correo;
+    private String contraseña;
+
+    /**
+     * @return the id
+     */
+    public Long getIdentificador() {
+        return identificador;
+    }
+    
+    /**
+     * @param identificador the id to set
+     */
+    public void setIdeentificador(Long identificador) {
+        this.identificador = identificador;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * @return the contraseña
+     */
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    /**
+     * @param contraseña the contraseña to set
+     */
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+}
