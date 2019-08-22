676f751d4179e8256d377dd5b4466358bbcd0663
a.despres@uniandes.edu.co <Estudiante@ISIS2603-127.sis.virtual.uniandes.edu.co>
2019-08-21 19:15:48 -0500

    Modificacion sobre la clase Memoria

---
+++ s2_eventos-back/src/main/java/co/edu/uniandes/csw/eventos/entities/MemoriaEntity.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.eventos.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Estudiante
+ */
+import java.util.Date;
+public class MemoriaEntity extends BaseEntity implements Serializable{
+    private String lugar;
+    private Date fecha;
+   
+    /**
+     * @return the lugar
+     */
+    public String getLugar(){
+        return this.lugar;
+    };
+    
+     /**
+     * @param newLugar the lugar to set
+     */
+    public void setLugar(String newLugar){
+        this.lugar=newLugar;
+    }
+    
+    /**
+     * @return the fecha
+     */
+    public Date getFecha(){
+        return this.fecha;
+    }
+    
+     /**
+     * @param newFecha the fecha to set
+     */
+    public void setFecha(Date newFecha){
+        this.fecha=newFecha;
+    }
+    
+    
+    
+}
