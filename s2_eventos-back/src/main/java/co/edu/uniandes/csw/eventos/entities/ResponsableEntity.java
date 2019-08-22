5a2d8401036a0f5e3ffb8062d32a0d08e80d95cd
a.despres@uniandes.edu.co <Estudiante@ISIS2603-127.sis.virtual.uniandes.edu.co>
2019-08-21 22:31:21 -0500

    Add ResponsableEntity

---
+++ s2_eventos-back/src/main/java/co/edu/uniandes/csw/eventos/entities/ResponsableEntity.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.eventos.entities;
+
+import javax.persistence.Entity;
+/**
+ *
+ * @author Estudiante
+ */
+@Entity
+public class ResponsableEntity extends UsuarioEntity {
+    private String codigoUniandes;
+    
+     /**
+     * @return the codigoUniandes
+     */
+    public String getCodigoUniandes(){
+        return this.codigoUniandes;
+    };
+    
+     /**
+     * @param newCodigo the codigo to set
+     */
+    public void setCodigoUniandes(String newCodigo){
+        this.codigoUniandes=newCodigo;
+    }
+    
+}
