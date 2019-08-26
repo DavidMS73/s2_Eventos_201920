f4aefe38d29115b01f94101273d9db1dc1b3bf51
Alberic <a.despres@uniandes.edu.co>
2019-08-26 18:11:54 -0500

    Modification memoria Persistence and Test

--- s2_eventos-back/src/main/java/co/edu/uniandes/csw/eventos/persistence/MemoriaPersistence.java
+++ s2_eventos-back/src/main/java/co/edu/uniandes/csw/eventos/persistence/MemoriaPersistence.java
@@ -5,10 +5,13 @@
  */
 package co.edu.uniandes.csw.eventos.persistence;
 
+import co.edu.uniandes.csw.eventos.entities.EventoEntity;
 import co.edu.uniandes.csw.eventos.entities.MemoriaEntity;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -23,5 +26,40 @@ public class MemoriaPersistence {
     public MemoriaEntity create(MemoriaEntity memoria) {
         em.persist(memoria);
         return memoria;
+    }
+    
+     public MemoriaEntity find(Long memoriasId) {
+        return em.find(MemoriaEntity.class, memoriasId);
+    }
+
+    public List<MemoriaEntity> findAll() {
+        TypedQuery<MemoriaEntity> query = em.createQuery("select u from MemoriaEntity u", MemoriaEntity.class);
+        return query.getResultList();
+    }
+    
+    public MemoriaEntity update(MemoriaEntity memoria) {
+        return em.merge(memoria);
+    }
+    
+    public void delete(Long memoriaId) {
+        MemoriaEntity entity = em.find(MemoriaEntity.class, memoriaId);
+        em.remove(entity);
+    }
+    
+    public MemoriaEntity findByName(String name){
+        TypedQuery query = em.createQuery("select u from MemoriaEntity u where u.lugar = :name", MemoriaEntity.class);
+        query = query.setParameter("name", name);
+        List<MemoriaEntity> sameName = query.getResultList();
+        MemoriaEntity result;
+        if(sameName == null) {
+            result = null;
+        }
+        else if(sameName.isEmpty()) {
+            result = null;
+        }
+        else{
+            result = sameName.get(0);
+        }
+        return result;
     }
 }
--- s2_eventos-back/src/test/java/co/edu/uniandes/csw/eventos/test/persistence/MemoriaPersistenceTest.java
+++ s2_eventos-back/src/test/java/co/edu/uniandes/csw/eventos/test/persistence/MemoriaPersistenceTest.java
@@ -5,17 +5,22 @@
  */
 package co.edu.uniandes.csw.eventos.test.persistence;
 
+import co.edu.uniandes.csw.eventos.entities.EventoEntity;
 import co.edu.uniandes.csw.eventos.entities.MemoriaEntity;
 import co.edu.uniandes.csw.eventos.persistence.MemoriaPersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -34,10 +39,46 @@ public class MemoriaPersistenceTest {
     }
     
     @Inject
+    UserTransaction utx;
+    
+    @Inject
     MemoriaPersistence mp;
     
     @PersistenceContext(unitName = "eventosPU")
     protected EntityManager em;
+    
+    private List<MemoriaEntity> data = new ArrayList<MemoriaEntity>();
+    
+    @Before
+    public void setUp() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    private void clearData() {
+        em.createQuery("delete from MemoriaEntity").executeUpdate();
+    }
+    
+     private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            MemoriaEntity entity = factory.manufacturePojo(MemoriaEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
     
     @Test
     public void testCreate(){
@@ -57,4 +98,61 @@ public class MemoriaPersistenceTest {
         
     }
     
+    
+    @Test
+    public void getMemoriasTest() {
+        List<MemoriaEntity> list = mp.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for(MemoriaEntity ent : list) {
+            boolean found = false;
+            for (MemoriaEntity entity : data) {
+                if (ent.getId().equals(entity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+    
+    @Test
+    public void getMemoriaTest(){
+        MemoriaEntity entity = data.get(0);
+        MemoriaEntity newEntity = mp.find(entity.getId());
+        Assert.assertNotNull(newEntity); 
+        Assert.assertEquals(entity.getLugar(), newEntity.getLugar());
+    }
+    
+    @Test
+    public void deleteMemoriaTest() {
+        MemoriaEntity entity = data.get(0);
+        mp.delete(entity.getId());
+        MemoriaEntity deleted = em.find(MemoriaEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+    
+    @Test
+    public void updateMemoriaTest() {
+        MemoriaEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        MemoriaEntity newEntity = factory.manufacturePojo(MemoriaEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        mp.update(newEntity);
+
+        MemoriaEntity resp = em.find(MemoriaEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getLugar(), resp.getLugar());
+    }
+    
+    @Test
+    public void findEventoByNameTest() {
+        MemoriaEntity entity = data.get(0);
+        MemoriaEntity newEntity = mp.findByName(entity.getLugar());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getLugar(), newEntity.getLugar());
+
+        newEntity = mp.findByName(null);
+        Assert.assertNull(newEntity);
+    }
 }
