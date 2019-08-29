1de50ddadddc75c5d87ea49b1be8b6d49ec9d863
Daniel Santiago Tenjo Leal <ds.tenjo@uniandes.edu.co>
2019-08-29 10:46:59 -0500

    tests melos

--- s2_eventos-back/src/test/java/co/edu/uniandes/csw/eventos/test/persistence/PagoPersistenceTest.java
+++ s2_eventos-back/src/test/java/co/edu/uniandes/csw/eventos/test/persistence/PagoPersistenceTest.java
@@ -5,16 +5,21 @@
  */
 package co.edu.uniandes.csw.eventos.test.persistence;
 
+
 import co.edu.uniandes.csw.eventos.entities.PagoEntity;
 import co.edu.uniandes.csw.eventos.persistence.PagoPersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -41,8 +46,50 @@ public class PagoPersistenceTest {
     @PersistenceContext
     EntityManager em;
     
+    @Inject
+    UserTransaction utx;
+    
+    private List<PagoEntity> data = new ArrayList<PagoEntity>();
+    
+    @Before
+    public void setUp() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    private void clearData() {
+        em.createQuery("delete from PagoEntity").executeUpdate();
+    }
+    
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            PagoEntity entity = factory.manufacturePojo(PagoEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
     @Test
-    public void createUsuarioTest() {
+    public void deletePagoTest() {
+        PagoEntity entity = data.get(0);
+        up.delete(entity.getId());
+        PagoEntity deleted = em.find(PagoEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+    
+    @Test
+    public void createPagoTest() {
         PodamFactory factory = new PodamFactoryImpl();
         PagoEntity pago = factory.manufacturePojo(PagoEntity.class);
         PagoEntity result = up.create(pago);
@@ -52,6 +99,20 @@ public class PagoPersistenceTest {
         PagoEntity entity = em.find(PagoEntity.class, result.getId());
 
         Assert.assertEquals(pago.getTitular(), entity.getTitular());
+    }
+    @Test
+    public void updatePagoTest() {
+        PagoEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        PagoEntity newEntity = factory.manufacturePojo(PagoEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        up.update(newEntity);
+
+        PagoEntity resp = em.find(PagoEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getCodigoConfirmacion(), resp.getCodigoConfirmacion());
     }
     
     
