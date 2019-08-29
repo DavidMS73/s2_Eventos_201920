e6ea38c113b72c26f8e6d5d07eeb1ed5842002d3
Samuel <cs.osorioa@uniandes.edu.co>
2019-08-28 21:04:39 -0500

    Se completaron las clases Tarjeta y MedioPago junto con sus respectivas pruebas.

---
+++ s2_eventos-back/src/main/java/co/edu/uniandes/csw/eventos/entities/TarjetaEntity.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.eventos.entities;
+
+import co.edu.uniandes.csw.eventos.podam.DateStrategy;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import uk.co.jemos.podam.common.PodamStrategyValue;
+
+/**
+ *
+ * @author Estudiante
+ */
+@Entity
+public class TarjetaEntity extends MedioPagoEntity implements Serializable{
+    private String tipoTarjeta;
+    
+    private String numeroTarjeta;
+    
+    @Temporal(TemporalType.DATE)
+    @PodamStrategyValue(DateStrategy.class)
+    private Date expiracion;
+    
+    private Integer cw;
+    /**
+     * Retorna el tipo de tarjeta.
+     * @return tipoTarjeta.
+     */
+    public String getTipoTarjeta(){
+        return tipoTarjeta;
+    }
+    
+    /**
+     * Establece el tipo de tarjeta.
+     * @param pTipo el tipo de tarjeta que se estblecerá.
+     */
+    public void setTipoTarjeta(String pTipo){
+        this.tipoTarjeta = pTipo;
+    }
+    
+    /**
+     * Retorna el numero de tarjeta.
+     * @return numeroTarjeta.
+     */
+    public String getNumeroTarjeta(){
+        return numeroTarjeta;
+    }
+    
+    /**
+     * Establece un nuevo numero de tarjeta.
+     * @param pNumero el nuevo numero de tarjeta que se establecerá.
+     */
+    public void setNumeroTarjeta(String pNumero){
+        this.numeroTarjeta = pNumero;
+    }
+    
+    /**
+     * Retorna la fecha de expiración.
+     * @return expiración.
+     */
+    public Date getExpiracion(){
+        return expiracion;
+    }
+    
+    /**
+     * Establece la fecha de expiración de un tarjeta.
+     * @param pExpiracion fecha de expiración que se establecerá.
+     */
+    public void setExpiracion(Date pExpiracion){
+        this.expiracion = pExpiracion;
+    }
+    
+    /**
+     * Retorna el cw.
+     * @return cw.
+     */
+    public Integer getCw(){
+        return cw;
+    }
+    
+    /**
+     * Establece un nuevo cw.
+     * @param pCw cw que se establecerá.
+     */
+    public void setCw(Integer pCw){
+        this.cw = pCw;
+    }
+}
---
+++ s2_eventos-back/src/test/java/co/edu/uniandes/csw/eventos/test/persistence/MedioPagoPersistenceTest.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.eventos.test.persistence;
+
+import co.edu.uniandes.csw.eventos.entities.MedioPagoEntity;
+import co.edu.uniandes.csw.eventos.persistence.MedioPagoPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author Estudiante
+ */
+@RunWith(Arquillian.class)
+public class MedioPagoPersistenceTest {
+    
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(MedioPagoEntity.class)
+                .addClass(MedioPagoPersistence.class)
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+     @Inject
+    UserTransaction utx;
+     
+    @Inject
+    private MedioPagoPersistence ep;
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    private List<MedioPagoEntity> data = new ArrayList<MedioPagoEntity>();
+
+    @Before
+    public void setUp() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    private void clearData() {
+        em.createQuery("delete from MedioPagoEntity").executeUpdate();
+    }
+    
+     private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            MedioPagoEntity entity = factory.manufacturePojo(MedioPagoEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+     
+      @Test
+    public void createMedioPagoTest() {
+        PodamFactory factory = new PodamFactoryImpl();
+        MedioPagoEntity medioPago = factory.manufacturePojo(MedioPagoEntity.class);
+        MedioPagoEntity result = ep.create(medioPago);
+        Assert.assertNotNull(result);
+
+        MedioPagoEntity entity = em.find(MedioPagoEntity.class, result.getId());
+
+        Assert.assertEquals(medioPago.getNumeroRecibo(), entity.getNumeroRecibo());
+    }
+    
+    @Test
+    public void deleteMedioPagoTest() {
+        MedioPagoEntity entity = data.get(0);
+        ep.delete(entity.getId());
+        MedioPagoEntity deleted = em.find(MedioPagoEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+    
+    @Test
+    public void getMedioPagoTest() {
+        List<MedioPagoEntity> list = ep.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for(MedioPagoEntity ent : list) {
+            boolean found = false;
+            for (MedioPagoEntity entity : data) {
+                if (ent.getId().equals(entity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+    
+    @Test
+    public void getEventoTest(){
+        MedioPagoEntity entity = data.get(0);
+        MedioPagoEntity newEntity = ep.find(entity.getId());
+        Assert.assertNotNull(newEntity); 
+        Assert.assertEquals(entity.getNumeroRecibo(), newEntity.getNumeroRecibo());
+    }
+    
+    @Test
+    public void updateMedioPagoTest() {
+        MedioPagoEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        MedioPagoEntity newEntity = factory.manufacturePojo(MedioPagoEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        ep.update(newEntity);
+
+        MedioPagoEntity resp = em.find(MedioPagoEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getNumeroRecibo(), resp.getNumeroRecibo());
+    }
+}
