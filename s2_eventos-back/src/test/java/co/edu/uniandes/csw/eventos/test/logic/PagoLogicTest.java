ce596be883990904af02fb6cb8b914c2cc091673
Daniel Santiago Tenjo Leal <ds.tenjo@uniandes.edu.co>
2019-09-03 10:55:29 -0500

    Pago Logic y PagologicTests adelantados

---
+++ s2_eventos-back/src/test/java/co/edu/uniandes/csw/eventos/test/logic/PagoLogicTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.eventos.test.logic;
+
+import co.edu.uniandes.csw.eventos.ejb.PagoLogic;
+import co.edu.uniandes.csw.eventos.entities.PagoEntity;
+import co.edu.uniandes.csw.eventos.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.eventos.persistence.PagoPersistence;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author El Daniel Santiago
+ */
+@RunWith(Arquillian.class)
+public class PagoLogicTest {
+    
+    @Deployment
+    public static JavaArchive CreateDeployment()
+    {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(PagoEntity.class.getPackage())
+                .addPackage(PagoLogic.class.getPackage())
+                .addPackage(PagoPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    
+   private PodamFactory factory= new PodamFactoryImpl();
+   
+   
+   @Inject
+   private PagoLogic pagoLogic;
+   
+   @PersistenceContext
+   private EntityManager em;
+           
+   @Test
+   public void createPagoTest() throws BusinessLogicException
+   {
+       PagoEntity newEntity= factory.manufacturePojo(PagoEntity.class);
+       PagoEntity result =pagoLogic.createPago(newEntity);
+       Assert.assertNotNull(result);
+   }
+   
+   @Test (expected= BusinessLogicException.class)
+   public  void createPagoFechaNullTest() throws BusinessLogicException
+   {
+       PagoEntity newEntity= factory.manufacturePojo(PagoEntity.class);
+       newEntity.setFecha(null);
+       PagoEntity result= pagoLogic.createPago(newEntity);
+   }
+           
+}
