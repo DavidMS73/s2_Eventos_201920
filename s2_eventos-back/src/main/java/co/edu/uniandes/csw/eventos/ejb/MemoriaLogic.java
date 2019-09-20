8d4d43a6a37f8bb5e104dfbe0a6d1a71e822b79d
Alberic <a.despres@uniandes.edu.co>
2019-09-20 18:17:54 -0500

    Finalizacion de MemoriaLogic

--- s2_eventos-back/src/main/java/co/edu/uniandes/csw/eventos/ejb/MemoriaLogic.java
+++ s2_eventos-back/src/main/java/co/edu/uniandes/csw/eventos/ejb/MemoriaLogic.java
@@ -8,6 +8,9 @@ package co.edu.uniandes.csw.eventos.ejb;
 import co.edu.uniandes.csw.eventos.entities.MemoriaEntity;
 import co.edu.uniandes.csw.eventos.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.eventos.persistence.MemoriaPersistence;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -18,6 +21,8 @@ import javax.inject.Inject;
 @Stateless
 public class MemoriaLogic {
 
+    private static final Logger LOGGER = Logger.getLogger(MemoriaLogic.class.getName());
+    
     @Inject
     private MemoriaPersistence persistence;
 
@@ -30,5 +35,34 @@ public class MemoriaLogic {
         memoria = persistence.create(memoria);
         return memoria;
     }
+    
+    public List<MemoriaEntity> getMemorias() {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar todos las memorias");
+        List<MemoriaEntity> memorias = persistence.findAll();
+        LOGGER.log(Level.INFO, "Termina proceso de consultar todos las memorias");
+        return memorias;
+    }
 
+    public MemoriaEntity getMemoria(Long memoriaId) {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar la memoria con id = {0}", memoriaId);
+        MemoriaEntity memoria = persistence.find(memoriaId);
+        if (memoria == null) {
+            LOGGER.log(Level.SEVERE, "La memoria con el id = {0} no existe", memoriaId);
+        }
+        LOGGER.log(Level.INFO, "Termina proceso de consultar la memoria con id = {0}", memoriaId);
+        return memoria;
+    }
+    
+    public MemoriaEntity updateMemoria(Long memoriaId, MemoriaEntity memoria) {
+        LOGGER.log(Level.INFO, "Inicia proceso de actualizar la memoria con id = {0}", memoriaId);
+        MemoriaEntity newEntity = persistence.update(memoria);
+        LOGGER.log(Level.INFO, "Termina proceso de actualizar la memoria con id = {0}", memoria.getId());
+        return newEntity;
+    }
+    
+     public void deleteMemoria(Long memoriaId) {
+        LOGGER.log(Level.INFO, "Inicia proceso de borrar la memoria con id = {0}", memoriaId);
+        persistence.delete(memoriaId);
+        LOGGER.log(Level.INFO, "Termina proceso de borrar la memoria con id = {0}", memoriaId);
+    }
 }
