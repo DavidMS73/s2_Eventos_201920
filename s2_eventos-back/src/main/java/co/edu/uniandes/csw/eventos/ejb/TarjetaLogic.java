97172abeeebba3f9001f8e5f3e2bbc179c287a65
Samuel <cs.osorioa@uniandes.edu.co>
2019-09-20 14:30:27 -0500

    Se terminó la lógica de tarjeta.

--- s2_eventos-back/src/main/java/co/edu/uniandes/csw/eventos/ejb/TarjetaLogic.java
+++ s2_eventos-back/src/main/java/co/edu/uniandes/csw/eventos/ejb/TarjetaLogic.java
@@ -8,6 +8,9 @@ package co.edu.uniandes.csw.eventos.ejb;
 import co.edu.uniandes.csw.eventos.entities.TarjetaEntity;
 import co.edu.uniandes.csw.eventos.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.eventos.persistence.TarjetaPersistence;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -17,17 +20,50 @@ import javax.inject.Inject;
  */
 @Stateless
 public class TarjetaLogic {
+    
+    private static final Logger LOGGER = Logger.getLogger(TarjetaLogic.class.getName());
 
     @Inject
     private TarjetaPersistence persistence;
 
     public TarjetaEntity createTarjeta(TarjetaEntity pTarjeta) throws BusinessLogicException {
-        if (pTarjeta.getNumeroTarjeta() == null) {
+        if (pTarjeta.getNumeroTarjeta() == null || pTarjeta.getNumeroTarjeta().length() != 16) 
             throw new BusinessLogicException("No existe el número de la tarjeta.");
-        } else {
-            pTarjeta = persistence.create(pTarjeta);
-            return pTarjeta;
-        }
+        if(pTarjeta.getCw() == null)
+            throw new BusinessLogicException("No existe el cw de la tarjeta.");
+        if(pTarjeta.getTipoTarjeta() == null)
+            throw new BusinessLogicException("El tipo de tarjeta no existe.");
+        if(pTarjeta.getExpiracion() == null)
+            throw new BusinessLogicException("La tarjeta no tiene fecha de expiración");
+       
+        pTarjeta = persistence.create(pTarjeta);
+        return pTarjeta;
+    }
+    
+    
+    public TarjetaEntity updateTarjeta(Long tarjetaId, TarjetaEntity pTarjeta) throws BusinessLogicException{
+        LOGGER.log(Level.INFO, "Inicia proceso de actualizar tarjeta con id = {0}", tarjetaId);
+        if(!validateNumero(pTarjeta.getNumeroTarjeta()))
+            throw new BusinessLogicException("El nuevo número de tarjeta es inválido.");
+        
+        TarjetaEntity update = persistence.update(pTarjeta);
+        LOGGER.log(Level.INFO, "Inicia proceso de actualizar tarjeta con id = {0}", pTarjeta.getId());
+        return pTarjeta;
+    }
+    
+    
+    public void deleteTarjeta(Long tarjetaId){
+        LOGGER.log(Level.INFO, "Inicia proceso de borrar la editorial con id = {0}", tarjetaId);
+        persistence.delete(tarjetaId);
+        LOGGER.log(Level.INFO, "Termina proceso de borrar la editorial con id = {0}", tarjetaId);
+    }
+    
+    public TarjetaEntity getTarjeta(Long tarjetaId) throws BusinessLogicException{
+        TarjetaEntity result = persistence.find(tarjetaId);
+        if(result == null)
+            throw new BusinessLogicException("No existe el medio de pagio que se introdujo.");
+        
+        return result;
     }
 
     public TarjetaEntity createTarjetaNumeroInvalido(TarjetaEntity pTarjeta) throws BusinessLogicException {
@@ -64,5 +100,17 @@ public class TarjetaLogic {
             pTarjeta = persistence.create(pTarjeta);
             return pTarjeta;
         }
+    }
+    
+    public List<TarjetaEntity> getTarjetas(){
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar todas las tarjetas");
+        List<TarjetaEntity> tarjetas = persistence.findAll();
+        LOGGER.log(Level.INFO, "Termina proceso de consultar todas las tarjetas");
+        return tarjetas;
+    }
+    
+    
+    private boolean validateNumero(String pNumero){
+        return !(pNumero == null || pNumero.isEmpty() || !(pNumero.length() == 16));
     }
 }
--- s2_eventos-back/src/test/java/co/edu/uniandes/csw/eventos/test/logic/TarjetaLogicTest.java
+++ s2_eventos-back/src/test/java/co/edu/uniandes/csw/eventos/test/logic/TarjetaLogicTest.java
@@ -9,14 +9,18 @@ import co.edu.uniandes.csw.eventos.ejb.TarjetaLogic;
 import co.edu.uniandes.csw.eventos.entities.TarjetaEntity;
 import co.edu.uniandes.csw.eventos.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.eventos.persistence.TarjetaPersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -24,7 +28,7 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 /**
  *
- * @author Samuel Osorio
+ * @author Samuelillo el pillo
  */
 @RunWith(Arquillian.class)
 public class TarjetaLogicTest {
@@ -46,19 +50,67 @@ public class TarjetaLogicTest {
 
     @Inject
     private TarjetaLogic tarjetaLogic;
+    
+    @Inject
+    private UserTransaction utx;
+    
+    private ArrayList<TarjetaEntity> data = new ArrayList<>();
+    
+     /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from TarjetaEntity").executeUpdate();
+    }
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+         for (int i = 0; i < 3; i++) {
+            TarjetaEntity tarjeta = factory.manufacturePojo(TarjetaEntity.class);
+            em.persist(tarjeta);
+            data.add(tarjeta);
+        }       
+    }
+
 
     @Test
-    public void createTarjeta() throws BusinessLogicException {
+    public void createTarjetaTest() throws BusinessLogicException {
         TarjetaEntity newEntity = factory.manufacturePojo(TarjetaEntity.class);
         TarjetaEntity result = tarjetaLogic.createTarjeta(newEntity);
         Assert.assertNotNull(result);
 
         TarjetaEntity entity = em.find(TarjetaEntity.class, result.getId());
         Assert.assertEquals(entity.getNumeroTarjeta(), result.getNumeroTarjeta());
+        Assert.assertEquals(entity.getCw(), result.getCw());
+        Assert.assertEquals(entity.getExpiracion(), result.getExpiracion());
+        Assert.assertEquals(entity.getTipoTarjeta(), result.getTipoTarjeta());
     }
 
     @Test(expected = BusinessLogicException.class)
-    public void createTarjetaNumNull() throws BusinessLogicException {
+    public void createTarjetaNumNullTest() throws BusinessLogicException {
         TarjetaEntity newEntity = factory.manufacturePojo(TarjetaEntity.class);
         newEntity.setNumeroTarjeta(null);
         TarjetaEntity result = tarjetaLogic.createTarjeta(newEntity);
@@ -79,14 +131,14 @@ public class TarjetaLogicTest {
     }
 
     @Test(expected = BusinessLogicException.class)
-    public void createTarjetaTipoNull() throws BusinessLogicException {
+    public void createTarjetaTipoNullTest() throws BusinessLogicException {
         TarjetaEntity newEntity = factory.manufacturePojo(TarjetaEntity.class);
         newEntity.setTipoTarjeta(null);
         TarjetaEntity result = tarjetaLogic.createTarjetaTipoNull(newEntity);
     }
 
     @Test(expected = BusinessLogicException.class)
-    public void createTarjetaCWInvalido() throws BusinessLogicException {
+    public void createTarjetaCWInvalidoTest() throws BusinessLogicException {
         TarjetaEntity newEntity = factory.manufacturePojo(TarjetaEntity.class);
         newEntity.setCw(null);
         TarjetaEntity result = tarjetaLogic.createTarjetaCWInvalido(newEntity);
@@ -97,5 +149,53 @@ public class TarjetaLogicTest {
         TarjetaEntity newEntity = factory.manufacturePojo(TarjetaEntity.class);
         newEntity.setExpiracion(null);
         TarjetaEntity result = tarjetaLogic.createTarjetaExpiracionInvalida(newEntity);
+    }
+    
+    @Test
+    public void updateTarjetaTest() throws BusinessLogicException{
+        TarjetaEntity entity = data.get(0);
+        TarjetaEntity pojoEntity = factory.manufacturePojo(TarjetaEntity.class);
+        pojoEntity.setId(entity.getId());
+        tarjetaLogic.updateTarjeta(pojoEntity.getId(), pojoEntity);
+        TarjetaEntity result = em.find(TarjetaEntity.class, entity.getId());
+        Assert.assertEquals(pojoEntity.getId(), result.getId());
+        Assert.assertEquals(pojoEntity.getNumeroTarjeta(), result.getNumeroTarjeta());
+        Assert.assertEquals(pojoEntity.getCw(), result.getCw());
+        Assert.assertEquals(pojoEntity.getExpiracion(), result.getExpiracion());
+        Assert.assertEquals(pojoEntity.getTipoTarjeta(), result.getTipoTarjeta());
+    }
+    
+    @Test
+    public void deleteTarjetaTest(){
+        TarjetaEntity result = data.get(0);
+        tarjetaLogic.deleteTarjeta(result.getId());
+        TarjetaEntity deleted = em.find(TarjetaEntity.class, result.getId());
+        Assert.assertNull(deleted);
+    }
+    
+    @Test
+    public void getTarjetaTest() throws BusinessLogicException{
+        TarjetaEntity entity = data.get(0);
+        TarjetaEntity result = tarjetaLogic.getTarjeta(entity.getId());
+        Assert.assertNotNull(result);
+        Assert.assertEquals(entity.getNumeroTarjeta(), result.getNumeroTarjeta());
+        Assert.assertEquals(entity.getCw(), result.getCw());
+        Assert.assertEquals(entity.getExpiracion(), result.getExpiracion());
+        Assert.assertEquals(entity.getTipoTarjeta(), result.getTipoTarjeta());
+    }
+    
+    @Test
+    public void getTarjetasTest(){
+        List<TarjetaEntity> list = tarjetaLogic.getTarjetas();
+        Assert.assertEquals(data.size(), list.size());
+        for (TarjetaEntity entity : list) {
+            boolean found = false;
+            for (TarjetaEntity storedEntity : data) {
+                if (entity.getId().equals(storedEntity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
     }
 }
