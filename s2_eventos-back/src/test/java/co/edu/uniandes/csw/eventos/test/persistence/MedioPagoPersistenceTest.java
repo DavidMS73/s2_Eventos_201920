130d7812a511f87b42fb4dd7dba7f2210c4d8a37
gj.gonzalez <gj.gonzalez@uniandes.edu.co>
2019-09-21 11:43:27 -0500

    Multimedia DTO, Multimedia Resource y Lugar resource + Correccion de los test que estaban fallando

--- s2_eventos-back/src/test/java/co/edu/uniandes/csw/eventos/test/persistence/MedioPagoPersistenceTest.java
+++ s2_eventos-back/src/test/java/co/edu/uniandes/csw/eventos/test/persistence/MedioPagoPersistenceTest.java
@@ -34,8 +34,8 @@ public class MedioPagoPersistenceTest {
     @Deployment
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
-                .addClass(MedioPagoEntity.class)
-                .addClass(MedioPagoPersistence.class)
+                .addPackage(MedioPagoEntity.class.getPackage())
+                .addPackage(MedioPagoPersistence.class.getPackage())
                 .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
                 .addAsManifestResource("META-INF/beans.xml", "beans.xml");
     }
