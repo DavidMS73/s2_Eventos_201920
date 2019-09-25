bd7f478ff56a9956cc0de2c761fcbeeaab92f4d6
Alberic <a.despres@uniandes.edu.co>
2019-09-25 18:44:07 -0500

    Add servicio get, Put and DEL lugar

--- s2_eventos-api/src/main/java/co/edu/uniandes/csw/eventos/resources/LugarResource.java
+++ s2_eventos-api/src/main/java/co/edu/uniandes/csw/eventos/resources/LugarResource.java
@@ -16,10 +16,14 @@ import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 
 /**
  *
@@ -74,5 +78,76 @@ public class LugarResource {
         List<LugarDTO> listLugares = listEntity2DetailDTO(logica.getLugares());
         LOGGER.log(Level.INFO, "LugarResource getBooks: output: {0}", listLugares);
         return listLugares;
+    }
+    
+    /**
+     * Busca el lugar con el id asociado recibido en la URL y lo devuelve.
+     *
+     * @param lugaresId Identificador del lugar que se esta buscando. Este debe
+     * ser una cadena de dígitos.
+     * @return JSON {@link LugarDTO} - El lugar buscado
+     * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
+     * Error de lógica que se genera cuando no se encuentra el lugar.
+     */
+    @GET
+    @Path("{lugaresId: \\d+}")
+    public LugarDTO getLugar(@PathParam("lugaresId") Long lugaresId) {
+        LOGGER.log(Level.INFO, "LugarResource getLugar: input: {0}", lugaresId);
+        LugarEntity lugarEntity = logica.getLugar(lugaresId);
+        if (lugarEntity == null) {
+            throw new WebApplicationException("El recurso /lugares/" + lugaresId + " no existe.", 404);
+        }
+        LugarDTO lugarDTO = new LugarDTO(lugarEntity);
+        LOGGER.log(Level.INFO, "LugarResource getLugar: output: {0}", lugarDTO);
+        return lugarDTO;
+    }
+
+    /**
+     * Actualiza el lugar con el id recibido en la URL con la información que se
+     * recibe en el cuerpo de la petición.
+     *
+     * @param lugaresId Identificador del lugar que se desea actualizar. Este debe
+     * ser una cadena de dígitos.
+     * @param lugar {@link LugarDTO} El lugar que se desea guardar.
+     * @return JSON {@link BookDetailDTO} - El lugar guardada.
+     * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
+     * Error de lógica que se genera cuando no se encuentra el lugar a
+     * actualizar.
+     * @throws BusinessLogicException {@link BusinessLogicExceptionMapper} -
+     * Error de lógica que se genera cuando no se puede actualizar el lugar.
+     */
+    @PUT
+    @Path("{lugaresId: \\d+}")
+    public LugarDTO updateLugar(@PathParam("lugaresId") Long lugaresId, LugarDTO lugar) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "LugarResource updateLugar: input: id: {0} , book: {1}", new Object[]{lugaresId, lugar});
+        lugar.setId(lugaresId);
+        if (logica.getLugar(lugaresId) == null) {
+            throw new WebApplicationException("El recurso /lugares/" + lugaresId + " no existe.", 404);
+        }
+        LugarDTO lugarDTO = new LugarDTO(logica.updateLugar(lugaresId, lugar.toEntity()));
+        LOGGER.log(Level.INFO, "LugarResource updateLugar: output: {0}", lugarDTO);
+        return lugarDTO;
+    }
+
+    /**
+     * Borra el lugar con el id asociado recibido en la URL.
+     *
+     * @param lugaresId Identificador del lugar que se desea borrar. Este debe ser
+     * una cadena de dígitos.
+     * @throws co.edu.uniandes.csw.eventos.exceptions.BusinessLogicException
+     * cuando el libro tiene autores asociados.
+     * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
+     * Error de lógica que se genera cuando no se encuentra el lugar.
+     */
+    @DELETE
+    @Path("{lugaresId: \\d+}")
+    public void deleteLugar(@PathParam("lugaresId") Long lugaresId) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "LugarResource deleteLugar: input: {0}", lugaresId);
+        LugarEntity entity = logica.getLugar(lugaresId);
+        if (entity == null) {
+            throw new WebApplicationException("El recurso /books/" + lugaresId + " no existe.", 404);
+        }
+        logica.deleteLugar(lugaresId);
+        LOGGER.info("LugarResource deleteLugar: output: void");
     }
 }
