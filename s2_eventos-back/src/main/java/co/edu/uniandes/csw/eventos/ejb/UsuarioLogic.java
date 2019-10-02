af4f8ed01166954e78d13ad03c322b2b35b8f971
gj.gonzalez <gj.gonzalez@uniandes.edu.co>
2019-10-02 10:51:57 -0500

    Correccion

--- s2_eventos-back/src/main/java/co/edu/uniandes/csw/eventos/ejb/UsuarioLogic.java
+++ s2_eventos-back/src/main/java/co/edu/uniandes/csw/eventos/ejb/UsuarioLogic.java
@@ -8,6 +8,9 @@ package co.edu.uniandes.csw.eventos.ejb;
 import co.edu.uniandes.csw.eventos.entities.UsuarioEntity;
 import co.edu.uniandes.csw.eventos.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.eventos.persistence.UsuarioPersistence;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -18,6 +21,8 @@ import javax.inject.Inject;
 @Stateless
 public class UsuarioLogic {
 
+    private static final Logger LOGGER = Logger.getLogger(EventoLogic.class.getName());
+    
     @Inject
     private UsuarioPersistence persistence;
 
@@ -42,5 +47,40 @@ public class UsuarioLogic {
         }
         usuario = persistence.create(usuario);
         return usuario;
+    }
+    
+    public List<UsuarioEntity> getUsuarios()
+    {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar todos los usuarios");
+        List<UsuarioEntity> usuarios = persistence.findAll();
+        LOGGER.log(Level.INFO, "Termina proceso de consultar todos los usuarios");
+        return usuarios;
+    }
+    
+    public UsuarioEntity getUsuario(Long usuariosId)
+    {
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar el usuario con id = {0}", usuariosId);
+        UsuarioEntity usuarioEntity = persistence.find(usuariosId);
+        if(usuarioEntity == null)
+        {
+            LOGGER.log(Level.INFO, "El usuario con el id = {0} no existe", usuariosId);
+        }
+        LOGGER.log(Level.INFO, "Termina proceso de consultar el usuario con id = {0}", usuariosId);
+        return usuarioEntity;
+    }
+    
+    public UsuarioEntity updateUsuario(Long usuariosId, UsuarioEntity usuarioEntity)
+    {
+        LOGGER.log(Level.INFO, "Inicia proceso de actualizar usuario con id = {0}", usuariosId);
+        UsuarioEntity newEntity = persistence.update(usuarioEntity);
+        LOGGER.log(Level.INFO, "Termina proceso de actualizar usuario con id = {0}", usuarioEntity.getId());
+        return newEntity;
+    }
+    
+    public void deleteUsuario(Long usuariosId)
+    {
+        LOGGER.log(Level.INFO, "Inicia proceso de borrar el evento con id = {0}", usuariosId);
+        persistence.delete(usuariosId);
+        LOGGER.log(Level.INFO, "Termina proceso de borrer el evento con id = {0}", usuariosId);
     }
 }
