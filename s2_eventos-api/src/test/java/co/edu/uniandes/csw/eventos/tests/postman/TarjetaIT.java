dedde26ea69a16a6051a3a2ac19c7de986efc449
Germán Martínez <gd.martinez@uniandes.edu.co>
2019-11-21 20:00:53 -0500

    Act 10.56

--- s2_eventos-api/src/test/java/co/edu/uniandes/csw/eventos/tests/postman/TarjetaIT.java
+++ s2_eventos-api/src/test/java/co/edu/uniandes/csw/eventos/tests/postman/TarjetaIT.java
@@ -24,7 +24,7 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public class TarjetaIT {
 
-    private static final String COLLECTION = "Tarjeta tests.postman_collection";
+    private static final String COLLECTION = "TarjetaResource.postman_collection";
 
     @Deployment(testable = true)
     public static WebArchive createDeployment() {
